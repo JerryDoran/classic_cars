85eb121ff374a38d8875ce79e067f6049a738ce3
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-08-24 11:49:26 -0400

    Finished image gallery section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.menu');
 const wrappers = Array.from(document.querySelectorAll('.wrapper'));
+const videos = Array.from(document.querySelectorAll('.video'));
 
 menu.addEventListener('click', () => {
   Array.from(document.querySelectorAll('.target')).forEach((item) => {
@@ -12,5 +13,14 @@ wrappers.forEach((item) => {
     Array.from(document.querySelectorAll('.target')).forEach((item) => {
       item.classList.remove('change');
     });
+  });
+});
+
+videos.forEach((video) => {
+  video.addEventListener('mouseover', () => {
+    video.play();
+  });
+  video.addEventListener('mouseout', () => {
+    video.pause();
   });
 });
